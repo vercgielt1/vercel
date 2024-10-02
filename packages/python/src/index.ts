b39031ccd44dde18e62c829548295b9c375f2fde
Tom Lienard <tom.lienrd@gmail.com>
2024-10-02 15:25:22 +0100

    Remove script path

--- packages/python/src/index.ts
+++ packages/python/src/index.ts
@@ -224,7 +224,6 @@ export const build: BuildV3 = async ({
     runtime: pythonVersion.runtime,
     environment: {
       AWS_LAMBDA_EXEC_WRAPPER: '/opt/rust/bootstrap',
-      SCRIPT_PATH: `${handlerPyFilename}.py`,
     },
     supportsResponseStreaming: true,
   });
